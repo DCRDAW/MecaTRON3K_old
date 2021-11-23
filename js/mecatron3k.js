95119ace8400d561e9f4b440f393d02d140e5825
DCRDAW <dcarrionrodriguez.guadalupe@alumnado.fundacionloyola.net>
2021-11-23 10:35:45 +0100

    v1 Final?

--- js/mecatron3k.js
+++ js/mecatron3k.js
@@ -31,6 +31,7 @@ class Juego{
     this.generadorPalabras = window.setInterval(this.generarPalabra.bind(this), 3000)
     this.animador = window.setInterval(this.vista.moverPalabras.bind(this.vista), 300)
     window.onkeypress = this.pulsar.bind(this)
+    this.sw=0
   }
 
   generarPalabra(){
@@ -47,6 +48,21 @@ class Juego{
   **/
   pulsar(evento){
     let letraPulsada = evento.key
+    
+    if(letraPulsada==' ' && this.sw==0){
+      evento.preventDefault();
+      clearInterval(this.generadorPalabras);
+      clearInterval(this.animador);
+      this.sw=1
+      letraPulsada=null
+      
+    }
+    if(letraPulsada==' ' && this.sw==1){
+      evento.preventDefault();
+      this.generadorPalabras = window.setInterval(this.generarPalabra.bind(this), 3000)
+      this.animador = window.setInterval(this.vista.moverPalabras.bind(this.vista), 300)
+      this.sw=0
+    }
     //Busco todas las palabras
     let palabras = this.divPrincipal.querySelectorAll('.palabra')
     for(let palabra of palabras){
@@ -62,6 +78,7 @@ class Juego{
         if (nodoTexto.nodeValue.length == 0){
           palabra.remove()
           this.modelo.sumarPunto()
+          this.vista.mandarPuntos(this.modelo.puntuacion)
         }
       }
       else{
@@ -79,6 +96,7 @@ class Juego{
 class Vista{
   constructor(){
     this.div = null   //Div donde se desarrolla el juego
+    this.puntuacion = new Puntuacion(0)
   }
   /**
     Dibuja el área de juego.
@@ -94,6 +112,14 @@ class Vista{
     div.classList.add('palabra')
     div.style.top = '0px'
     div.style.left = Math.floor(Math.random() * 85) + '%'
+  }
+  /**
+   * envia los puntos a la clase puntuacion para que los muestre
+   * @param {*} puntos los puntos que el jugador tiene en ese momento
+   */
+  mandarPuntos(puntos){
+    this.puntuacion.puntua=puntos
+    this.puntuacion.mostrar()
   }
   /**
     Mueve las palabras del Juego
@@ -119,7 +145,13 @@ class Vista{
 **/
 class Modelo{
   constructor(){
-      this.palabras = ['En', 'un', 'lugar', 'de', 'La', 'Mancha']
+      this.nivel= 1;
+      this.puntosPasarNivel= 5;
+      this.palabras = [
+          ['ju','fr','fv','jm','fu','jr','jv','fm'],
+          ['fre','jui','fui','vie','mi','mery','huy'],
+          ['juan','remo','foca','dedo','cate']
+        ]
       this.puntuacion = 0
   }
   /**
@@ -128,11 +160,66 @@ class Modelo{
     @return {String} Palabra generada
   **/
   crearPalabra(){
-    return this.palabras[Math.floor(Math.random() * this.palabras.length)]
+    switch (this.nivel) {
+      case 1:
+        return this.palabras[0][Math.floor(Math.random() * 8)]
+        break;
+      case 2:
+        return this.palabras[1][Math.floor(Math.random() * 7)]
+        break;
+      case 3:
+        return this.palabras[2][Math.floor(Math.random() * 5)]
+        break;
+      default:
+        return this.palabras[2][Math.floor(Math.random() * 5)]
+        break;
+    }
+    
   }
+  /**
+   * suma 1 punto cada vez que es llamada
+   */
   sumarPunto(){
     this.puntuacion++
+   /* let puntos=document.getElementById('puntuacion')
+    puntos.textContent=`Puntuacion= ${this.puntuacion}`
+    */
+
+    this.comprobarNivel;
   }
+  /**
+   * comprueba si el jugador tiene los puntos suficientes como para subir de nivel
+   */
+  comprobarNivel(){
+    if(this.puntuacion>this.puntosPasarNivel){
+      let textoLvl= document.getElementById('nivel')
+      this.nivel++
+      this.puntosPasarNivel=this.puntosPasarNivel+5
+      textoLvl.textContent=`Nivel= ${this.nivel}`
+    }
+    if(this.puntuacion<this.puntosPasarNivel-5){
+      let textoLvl= document.getElementById('nivel')
+      this.nivel--
+      this.puntosPasarNivel=this.puntosPasarNivel-5
+      textoLvl.textContent=`Nivel= ${this.nivel}`
+    }
+  }
+ 
 }
+/**
+ * clase Puntuacion que almacena y muestra los puntos
+ */
+class Puntuacion{
+  constructor(punto){
+    this.puntua=punto
+  }
+  /**
+   * muestra la puntuacion por pantalla
+   */
+  mostrar(){
+    let puntos=document.getElementById('puntuacion')
+    puntos.textContent=`Puntuacion= ${this.puntua}`
+  }
+}   
 
 var app = new Juego()
